85404532a8ad6d40a91134e00866f03fed5a7a36
cevahir72 <kelesmevlut71@gmail.com>
2022-03-20 17:27:55 +0300

    admin-update-2

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Category,Like,Comment,Post,PostView
+from .models import Like,Comment,Post,PostView
 
-admin.site.register(Category)
+
 admin.site.register(Like)
 admin.site.register(Comment)
 admin.site.register(Post)
