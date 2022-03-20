a21cb3043470ee49ac3881d7caf68504b77484c6
cevahir72 <kelesmevlut71@gmail.com>
2022-03-20 17:27:04 +0300

    url-update

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from blog.views import PostListView
+from blog.views import ( PostListView,DetailView,CommentView,LikeView,PostViewList)
 
-urlpatterns=[
-    path("", PostListView.as_view()),
-    # path("<category>/", QuizView.as_view()),
-    # path("<category>/<quiz>/", QuestionView.as_view())
-    ]
+urlpatterns = [
+    path('', PostListView.as_view(), name='post-list'),
+    path('<int:id>/', DetailView.as_view(), name='post-detail'),
+    path('<int:id>/comment/', CommentView.as_view()),
+    path('<int:id>/like/', LikeView.as_view()),
+    path('<int:id>/view/', PostViewList.as_view()),
+]
